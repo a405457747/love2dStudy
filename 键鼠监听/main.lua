eaad6c9a625341d6f51500541e9d4e956b596a94
Sky.Allen <894982165@qq.com>
2024-07-11 07:00:18 +0800

    1

---
+++ 键鼠监听/main.lua
@@ -0,0 +1,13 @@
+
+function love.load()
+    
+end
+
+function love.update()
+end
+
+function  love.draw()
+end
+function love.keypressed(key,scancode,isrepeat)
+    print("key pressed",key,scancode, isrepeat )
+end
