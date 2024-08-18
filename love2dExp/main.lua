33aa09c056800de3a46c2f82f1be18525c4c416a
Sky.Allen <894982165@qq.com>
2024-08-19 06:43:40 +0800

    1

--- love2dExp/main.lua
+++ love2dExp/main.lua
@@ -1,8 +1,19 @@
-function love.draw()
-      love. graphics.setColor({255,255,0,255})
-      love.graphics.circle("fill",100,100,50);
-      love.graphics.rectangle("line",200,200,20,50);
-      --love.graphics.scale(2,10);
 
-      love.graphics.rectangle("line",300,300,20,50);
+imgs=nil
+function love.load()
+      imgs=love.graphics.newImage("jump_3.png");
+      --50,60是左上切割点坐标，150,100是切割宽高
+      qua1=love.graphics.newQuad(50,60,150,100,imgs:getWidth(),imgs:getHeight());
+end;
+function love.draw()
+      love.graphics.draw(imgs,0,0);
+      --200,300是坐标，qua1的quad对象
+      love.graphics.draw(imgs,qua1,200,300);
+end;
+
+function love.keypressed(key)
+      if key=='a' then
+            love.event.quit("restart");
+            print("a");
+      end;
 end
--- love2dLuogu/main.lua
+++ love2dLuogu/main.lua
@@ -1,45 +1,18 @@
-
-str =io.stdin:read("*line")
-
-t={}
-for item in str:gmatch("%S+") do
-    t[#t+1]=tonumber(item);
-end
-local n,m =unpack(t);
-
-local data={}
-for i=1,n do
-    local str =io.stdin:read("*line");
-    local  t={}
-    for item in str:gmatch("%S+") do
-        t[#t+1]=tonumber(item);
-    end
-    data[#data+1]=t;
-end;
-local function printTable(t)
-    print(table.concat(t," "));
+if arg[2] == "debug" then
+    require("lldebugger").start()
 end
 
-local function printMatrix(t)
-    for i, v in ipairs(t) do
-        printTable(v);
-    end
-end
 
-local function matrixT(data)
-    local o_r=#data;
-    local o_l=#data[1];
-    local n_r =o_l;
-    local n_l=o_r;
-    local new_data={}
-    for i=1,n_r do
-        local new_data_item={}
-        for j=1,n_l do
-            table.insert(new_data_item,data[j][i]);
+function love.load()
+    local t ={'a',"a",'b','c',"a","e",'a','k'}
+
+
+    for i=#t,1,-1 do
+        local item =t[i];
+        if item =='a' then
+            table.remove(t,i);
         end;
-        new_data[#new_data+1]=new_data_item;
     end;
-    return new_data;
+    print(unpack(t));
 end;
 
-printMatrix(matrixT(data));
