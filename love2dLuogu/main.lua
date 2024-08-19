c414444d4c71154167e41dcc95b20d11e01e2ee1
Sky.Allen <894982165@qq.com>
2024-08-19 19:14:44 +0800

    1

--- love2dLuogu/main.lua
+++ love2dLuogu/main.lua
@@ -1,18 +1,2 @@
-if arg[2] == "debug" then
-    require("lldebugger").start()
-end
-
-
-function love.load()
-    local t ={'a',"a",'b','c',"a","e",'a','k'}
-
-
-    for i=#t,1,-1 do
-        local item =t[i];
-        if item =='a' then
-            table.remove(t,i);
-        end;
-    end;
-    print(unpack(t));
-end;
-
+local socket = require("socket");
+print(type(socket),socket);
